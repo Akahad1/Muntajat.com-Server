643f6ca636d0959946f541baadd85e8efd45c2b8
Akahad <aksahad234567@gmail.com>
2023-09-30 10:47:45 +0600

    sort complite

--- index.js
+++ index.js
@@ -34,27 +34,43 @@ async function run() {
     const mobileCollction = client.db('Muntajat').collection('Mobile')
     const tabCollction = client.db('Muntajat').collection('Tab')
     const AllproducCollction = client.db('Muntajat').collection('AllProduct')
+    const AllCatagoreyproducCollction = client.db('Muntajat').collection('AllCatagoryProduct')
 
-    app.get('/laptop',async (req,res)=>{
-        const qurey={}
-        const result=await laptopCollction.find(qurey).toArray()
-        res.send(result)
-    })
+
     app.get('/allproduct',async (req,res)=>{
-        const qurey={}
-        const result=await AllproducCollction.find(qurey).toArray()
-        res.send(result)
-    })
-    app.get('/mobile',async(req,res)=>{
       const qurey={}
-      const result= await mobileCollction.find(qurey).toArray()
+      const result=await AllproducCollction.find(qurey).toArray()
       res.send(result)
-    })
-    app.get('/tab',async(req,res)=>{
-      const qurey={}
-      const result= await tabCollction.find(qurey).toArray()
+  })
+  app.get('/catagoryproduct',async (req,res)=>{
+
+
+   
+
+    const catagory=req.query.catagory
+      const qurey={category:catagory}
+      const result=await (await AllCatagoreyproducCollction.find(qurey).sort({price:1}).toArray())
       res.send(result)
-    })
+  })
+
+
+
+    // app.get('/laptop',async (req,res)=>{
+    //     const qurey={}
+    //     const result=await laptopCollction.find(qurey).toArray()
+    //     res.send(result)
+    // })
+    
+    // app.get('/mobile',async(req,res)=>{
+    //   const qurey={}
+    //   const result= await mobileCollction.find(qurey).toArray()
+    //   res.send(result)
+    // })
+    // app.get('/tab',async(req,res)=>{
+    //   const qurey={}
+    //   const result= await tabCollction.find(qurey).toArray()
+    //   res.send(result)
+    // })
     
     
   } finally {
